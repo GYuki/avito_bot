3b6ab7f859761024a29b7423d8589f3e1d58b95d
seri <seri05@yandex.ru>
2018-05-12 15:34:37 +0300

    Добавил модели

--- parserapp/models.py
+++ parserapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Chat(models.Model):
+    telegram_chat_id = models.IntegerField()
+
+class Category(models.Model):
+    name = models.Charfield(max_length=30)
+
+class CategorySubscriber(models.Model):
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+class Advert(models.Model):
+    avito_id = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
