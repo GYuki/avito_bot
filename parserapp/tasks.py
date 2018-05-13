8f6617d5ff215d9b45d4abdd3f5d12db76677e52
seri <seri05@yandex.ru>
2018-05-13 19:27:30 +0300

    Настроил отправку сообщений клиентам

--- parserapp/tasks.py
+++ parserapp/tasks.py
@@ -3,9 +3,10 @@ from __future__ import absolute_import, unicode_literals
 from bs4 import BeautifulSoup
 import requests
 import datetime
+from parserapp.bot_settings import url_token
 
 from parserapp import celery_app as app
-from parserapp.models import Category, CategorySubscriber, Advert
+from parserapp.models import Category, CategorySubscriber, Advert, Chat
 
 def find_subscribers(category_id):
     sub_arr = []
@@ -22,6 +23,11 @@ def initialize_tasks():
          'schedule': datetime.timedelta(seconds=20),
          'args': (i['id'],)
          }
+    schedule['telegram_sender'] = {
+        'task': 'parserapp.tasks.find_new_ads',
+         'schedule': datetime.timedelta(seconds=20),
+         'args': None
+    }
     app.conf.update(CELERYBEAT_SCHEDULE=schedule)
 
 def price_check(price):
@@ -30,6 +36,26 @@ def price_check(price):
     except:
         pass
     return ""
+
+@app.task
+def find_new_ads():
+    sub = Advert.objects.filter(is_sent=False, \
+    category__categorysubscriber__chat_id__isnull=False)\
+    .values('category__categorysubscriber__chat__telegram_chat_id', \
+    'title', 'price')
+    #print (sub)
+    for s in sub:
+        text = '%s %s' %(s['title'][2:-2], s['price'][1:])
+        send_message.delay(s['category__categorysubscriber__chat__telegram_chat_id'], text)
+    sub.update(is_sent=True)
+
+
+@app.task
+def send_message(chat_id, text):
+    URL = url_token + 'sendMessage'
+    answer = {'chat_id': chat_id, 'text': text}
+    r = requests.post(URL, json=answer)
+    return r.json()
 
 
 
@@ -56,7 +82,6 @@ def filter_ads(ads_dict, category_id):
     res = list(set(avito_ids) - set(sent_ads))
     for item in ads_dict:
         if int(item['avito_id']) in res:
-            print (item['price'])
             new_ad = Advert(avito_id=item['avito_id'], category_id=category_id,\
                             title=item['title'], price=item['price'])
             new_ad.save()
